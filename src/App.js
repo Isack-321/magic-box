0ebd00c0f85b538f1b83b3d4c0d8792948f73107
Isack <ituwei21@gmail.com>
2021-10-27 18:32:45 +0300

    added ui functionality

--- src/App.js
+++ src/App.js
@@ -32,7 +32,22 @@ console.log(cards,turns)
     <div className="App">
     <h1>Magic match</h1>
     <Button variant ="outlined" onClick={shufflecards}>New Game</Button>
+
+    
+    <div className="card-grid">
+        {cards.map(card=>(
+          <div key =
+          {card.id} className="card-details">
+            <div>
+              <img className="front" src={card.src} alt ="card front"/>
+              <img className="back" src="/img/cover.png" alt ="card back"/>
+              </div>      
+          </div>
+
+        ))}
+        </div>
       </div>
+
   );
 }
 
