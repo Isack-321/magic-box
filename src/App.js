86e83db817ced631a48a178a359c499637ae919f
Isack <ituwei21@gmail.com>
2021-10-27 17:15:07 +0300

    initialized project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Button from '@mui/material/Button';
+
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Magic match</h1>
+    <Button>New Game</Button>
+      </div>
+  );
+}
+
+export default App;
